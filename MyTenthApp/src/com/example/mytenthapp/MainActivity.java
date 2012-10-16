21f93c0e9c38b2e5b16a2c6902c974812e325dab
Preston Hardy <pghardy@csbsju.edu>
2012-10-15 19:51:31 -0500

    added 5 linked pages for rapid prototype

--- MyTenthApp/src/com/example/mytenthapp/MainActivity.java
+++ MyTenthApp/src/com/example/mytenthapp/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.mytenthapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 
@@ -15,16 +14,10 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
     
     /** Called when the user clicks the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
+    public void sendMessage2(View view) {
+        Intent intent = new Intent(this, Page2Activity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
